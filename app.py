2047f03253f746f5a82a0a29e4f181a48646cdbb
emanuele-code <emanuele.pacilio0@gmail.com>
2025-07-14 16:06:21 +0200

    creation of list page you can check on films that you have watched or that you want to watch

--- app.py
+++ app.py
@@ -10,5 +10,47 @@ def home():
 def registrazione():
     return render_template('registrazione.html')
 
+@app.route('/lista')
+def lista():
+    film_list = [
+        {
+            'titolo': '28 Years Later',
+            'stelle': 4,
+            'regista': 'Alex Garland',
+            'uscita': '2025',
+            'stato': 'visto',
+            'genere': 'Horror',
+            'immagine': 'images/28-Years-Later.jpg'
+        },
+        {
+            'titolo': 'Elio',
+            'stelle': 3,
+            'regista': 'Adrian Molina',
+            'uscita': '2025',
+            'stato': 'da-vedere',
+            'genere': 'Animazione',
+            'immagine': 'images/elio.jpg'
+        },
+        {
+            'titolo': 'Sorry, Baby',
+            'stelle': 5,
+            'regista': 'Emma Seligman',
+            'uscita': '2024',
+            'stato': 'visto',
+            'genere': 'drammatico',
+            'immagine': 'images/sorry-baby.jpg'
+        },
+        {
+            'titolo': 'Real steel',
+            'stelle': 4,
+            'regista': 'Shawn Levy',
+            'uscita': '2011',
+            'stato': 'visto',
+            'genere': 'Sci-Fi',
+            'immagine': 'images/real-steel.jpg'
+        }
+    ]
+    return render_template('lista.html', film_list=film_list)
+
 if __name__ == "__main__":
     app.run(debug=True)
