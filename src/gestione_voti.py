e38b5de7d061a9e31c7f7da4172ce3b0af9d2fe2
Emanuele Pacilio <emanuele.pacilio0@gmail.com>
2025-07-19 23:21:32 +0200

    added animation for star score in javascript set-stars.js

--- src/gestione_voti.py
+++ src/gestione_voti.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 
 voti_bp = Blueprint('voti', __name__)
 
+
 @voti_bp.route('/', methods=['GET', 'POST'])
 def voti():
 
